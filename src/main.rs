1d88663068f14b92a712e95ba7b23af8d53fcff4
Masahiro Kozuka <masa.koz@kozuka.jp>
2023-12-28 00:13:40 +0000

    implement flight's do_exchange.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,126 @@
-fn main() {
-    println!("Hello, world!");
+use arrow_flight::error::FlightError;
+use futures::stream::BoxStream;
+use futures::TryStreamExt;
+use tokio::sync::mpsc;
+use tokio::task;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::transport::Server;
+use tonic::{Request, Response, Status, Streaming};
+
+use arrow::ipc::reader::StreamReader;
+use arrow_flight::encode::FlightDataEncoderBuilder;
+use arrow_flight::{
+    flight_service_server::FlightService, flight_service_server::FlightServiceServer, Action,
+    ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo, HandshakeRequest,
+    HandshakeResponse, PutResult, SchemaResult, Ticket,
+};
+use std::io::BufReader;
+use std::process::{Command, Stdio};
+
+#[derive(Clone)]
+pub struct FlightServiceImpl {}
+
+#[tonic::async_trait]
+impl FlightService for FlightServiceImpl {
+    type HandshakeStream = BoxStream<'static, Result<HandshakeResponse, Status>>;
+    type ListFlightsStream = BoxStream<'static, Result<FlightInfo, Status>>;
+    type DoGetStream = BoxStream<'static, Result<FlightData, Status>>;
+    type DoPutStream = BoxStream<'static, Result<PutResult, Status>>;
+    type DoActionStream = BoxStream<'static, Result<arrow_flight::Result, Status>>;
+    type ListActionsStream = BoxStream<'static, Result<ActionType, Status>>;
+    type DoExchangeStream = BoxStream<'static, Result<FlightData, Status>>;
+
+    async fn handshake(
+        &self,
+        _request: Request<Streaming<HandshakeRequest>>,
+    ) -> Result<Response<Self::HandshakeStream>, Status> {
+        Err(Status::unimplemented("Implement handshake"))
+    }
+
+    async fn list_flights(
+        &self,
+        _request: Request<Criteria>,
+    ) -> Result<Response<Self::ListFlightsStream>, Status> {
+        Err(Status::unimplemented("Implement list_flights"))
+    }
+
+    async fn get_flight_info(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<FlightInfo>, Status> {
+        Err(Status::unimplemented("Implement get_flight_info"))
+    }
+
+    async fn get_schema(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<SchemaResult>, Status> {
+        Err(Status::unimplemented("Implement get_schema"))
+    }
+
+    async fn do_get(
+        &self,
+        _request: Request<Ticket>,
+    ) -> Result<Response<Self::DoGetStream>, Status> {
+        Err(Status::unimplemented("Implement do_get"))
+    }
+
+    async fn do_put(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoPutStream>, Status> {
+        Err(Status::unimplemented("Implement do_put"))
+    }
+
+    async fn do_action(
+        &self,
+        _request: Request<Action>,
+    ) -> Result<Response<Self::DoActionStream>, Status> {
+        Err(Status::unimplemented("Implement do_action"))
+    }
+
+    async fn list_actions(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<Self::ListActionsStream>, Status> {
+        Err(Status::unimplemented("Implement list_actions"))
+    }
+
+    async fn do_exchange(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoExchangeStream>, Status> {
+        let (tx, rx) = mpsc::channel(1);
+        task::spawn_blocking(move || -> Result<(), Status> {
+            let mut child = Command::new("target/debug/arrow_stdout")
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to execute command");
+            let stdout = child.stdout.take().expect("Failed to open stdout");
+            let mut reader = StreamReader::try_new(BufReader::new(stdout), None)
+                .expect("Failed to create StreamReader");
+            while let Some(batch) = reader.next() {
+                let batch = batch.map_err(|e| FlightError::Arrow(e));
+                tx.blocking_send(batch).expect("send error");
+            }
+            Ok(())
+        });
+
+        let stream = FlightDataEncoderBuilder::new()
+            .build(ReceiverStream::new(rx))
+            .map_err(Status::from);
+        Ok(Response::new(Box::pin(stream)))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50051".parse()?;
+    let service = FlightServiceImpl {};
+
+    let svc = FlightServiceServer::new(service);
+
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
 }
